4eb62576768f8b9fa179316a587b73a06d55c848
thantos666 <thomas.arrais@satom.ch>
2025-02-03 09:31:48 +0100

    ypdate

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -7,8 +7,12 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class BookController extends AbstractController{
-    #[Route('/book', name: 'app_book')]
-    public function index(): Response
+   #[Route('/book', name: 'book_single')]
+public function single(): Response
+{
+    return new Response('Page d\'un seul livre');
+}
+
     {
         return $this->render('book/index.html.twig', [
             'controller_name' => 'BookController',
