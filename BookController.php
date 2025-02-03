4a6604cea69c97ef02080bbdc6e173f0f0bdcb08
thantos666 <thomas.arrais@satom.ch>
2025-02-03 10:20:39 +0100

    update

--- BookController.php
+++ BookController.php
@@ -2,14 +2,39 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\BookService;
+use App\Form\BookType;
 
 class BookController extends AbstractController
 {
-    
+    private BookService $bookService;
+
+    public function __construct(BookService $bookService)
+    {
+        $this->bookService = $bookService;
+    }
+
+    #[Route('/books', name: 'book_list')]
     public function list(): Response
     {
-        return $this->render('book/index.html.twig');
+        $books = $this->bookService->getAllBooks();
+        return $this->render('book/index.html.twig', ['books' => $books]);
+    }
+
+    #[Route('/book/new', name: 'book_new')]
+    public function new(Request $request): Response
+    {
+        $form = $this->createForm(BookType::class);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->bookService->addBook($form->getData());
+            return $this->redirectToRoute('book_list');
+        }
+        
+        return $this->render('book/new.html.twig', ['form' => $form->createView()]);
     }
 }
